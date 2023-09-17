8e13e7bb3ab438b704f33da7adacf1f5bb0f3973
pedro <pedro.jcaceresl@gmail.com>
2023-09-17 16:29:25 -0400

    added twilio

--- server.js
+++ server.js
@@ -19,11 +19,37 @@ app.use(
   })
 );
 
+const accountSid = "ACc1f6d1e31f0f1370d8b7f2dd722ab091";
+const authToken = "77e793790529c4f25ead3c4fe60cabaf";
+const client = require("twilio")(accountSid, authToken);
+
+
 app.use("/messenger", require("./Facebook/facebookBot"));
 
 app.get("/", (req, res) => {
   return res.send("Chatbot Funcionando 🤖🤖🤖");
 });
+
+app.post("/whatsapp", (req, res) => {
+  const message = req.body;
+  // Do whatever you want with the message
+
+  
+client.messages
+	.create({
+		body: res.message,
+		from: "whatsapp:+14155238886",
+		to: "whatsapp:+595985586401",
+	})
+	.then((message) => console.log(message.sid))
+	.done();
+
+
+  res.send({
+    ok: true,
+    message: "Received message"
+  })
+})
 
 app.listen(port, () => {
   console.log(`Escuchando peticiones en el puerto ${port}`);
